5e12d1b13bed284abeb3af9b9e0c39a065504eb1
irbansin <reach@anirbansinha.in>
2026-03-09 15:51:45 +0530

    cors error rectified

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import Fastify from "fastify";
 import dotenv from "dotenv";
+import cors from "@fastify/cors";
+
 dotenv.config();
 
 const fastify = Fastify({ logger: true });
@@ -8,6 +10,24 @@ const PORT = process.env.PORT || 3000;
 const HOST =
   process.env.HOST ||
   (process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1");
+// CORS setup
+const CORS_ORIGIN = (process.env.CORS_ORIGIN || "")
+  .split(",")
+  .map((s) => s.trim())
+  .filter(Boolean);
+
+fastify.register(cors, {
+  origin: (origin, cb) => {
+    // allow non-browser tools (curl, server-to-server) when origin is undefined
+    if (!origin) return cb(null, true);
+    // if no whitelist defined, allow all (dev / simple setup)
+    // if (CORS_ORIGIN.length === 0) return cb(null, true);
+    // allow if origin is in whitelist
+    if (CORS_ORIGIN.includes(origin)) return cb(null, true);
+    // otherwise block
+    cb(new Error("Not allowed by CORS"), false);
+  },
+});
 
 // 1. A simple GET endpoint (Reading data)
 fastify.get("/api/v1/greet", async (request, reply) => {
